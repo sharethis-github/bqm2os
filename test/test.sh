9e682fa4905060d157815445399efd1772d9e8d4
Douglas Campbell <douglas@sharethis.com>
2018-01-06 11:01:09 -0800

    Allow unused template vars (#30)
    * [remove_gcs_resource-] remove gcs loader
    
    * [remove_gcs_resource-] commiting

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 for file in $(find /python -name '*.py')
 do
-    pycodestyle $file
+    pycodestyle --max-line-length=100 $file
     testfile=$(echo $file | awk -F/ '{printf "/test/"; n=3; while (n < NF) { printf $n"/"; n++}; print "test_"$NF}')
     coverage run -a --source=/python $testfile
     coverage report -m
